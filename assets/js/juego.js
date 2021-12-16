1005906265518f62499b083713ac85b8fdf366ee
betcorg <betcorg@gmail.com>
2021-12-16 13:28:15 -0600

    Agrego proyecto 1, juego de blackjack 21

---
+++ assets/js/juego.js
@@ -0,0 +1,23 @@
+
+
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S']; 
+
+const crearDeck = () => {
+
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos){
+            deck.push(i + tipo);
+
+        }
+        
+    }
+
+    console.log(deck);
+} 
+
+
+crearDeck();
+
+
