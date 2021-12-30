94bf48671378a8ddffd6afee2c0abef4519c18b3
betcorg <betcorg@gmail.com>
2021-12-30 08:23:09 -0600

    Agrego funcionalidad del botón pedir

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,9 +6,15 @@ let deck         = [];
 const tipos      = ['C', 'D', 'H', 'S']; 
 const especiales = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0;
+    puntosComputadora = 0;
+    
+
 
 // Referencias de HTML
 const btnPedir = document.querySelector('#btnPedir');
+const marcador = document.querySelectorAll('small');
+const divCartasJugador = document.querySelector('#jugador-cartas');
 
 
 // Esta función crea un deck o set de barajas
@@ -64,8 +70,31 @@ const valorCarta = (carta) => {
 
 
 // Eventos
+
+//Funciones del botón pedir
 btnPedir.addEventListener('click', () => {
 
-})
+    //Retira la carta del deck y hace la sumatoria en el marcador del jugador
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta(carta);
+    marcador[0].innerHTML = puntosJugador;
+    
+    // Crea la imagen de las cartas y las agrega al tablero
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
 
+    //Valida el puntaje y desactiva el botón pedir
+    if (puntosJugador > 21 ) {
+        console.warn('Lo siento, perdiste');
+        btnPedir.disabled = true;
+    } else if (puntosJugador === 21) {
+        console.warn('¡Genial, tienes 21 puntos!');
+        btnPedir.disabled = true;
+    }
+
+
+    
+})
 
