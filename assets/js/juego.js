b2d65433ab726603841b3669b235e5d6fb3d453c
betcorg <betcorg@gmail.com>
2021-12-30 18:09:18 -0600

    Agrego el código de la lógica de la computadora

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,15 +6,18 @@ let deck         = [];
 const tipos      = ['C', 'D', 'H', 'S']; 
 const especiales = ['A', 'J', 'Q', 'K'];
 
-let puntosJugador = 0;
+let puntosJugador = 0,
     puntosComputadora = 0;
     
 
 
 // Referencias de HTML
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const btnNuevo = document.querySelector('#btnNuevo');
 const marcador = document.querySelectorAll('small');
 const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
 
 
 // Esta función crea un deck o set de barajas
@@ -40,6 +43,8 @@ const crearDeck = () => {
     return deck;
 } 
 
+
+
 crearDeck();
 
 // Función que retira una carta del deck.
@@ -55,6 +60,7 @@ const pedirCarta = () => {
 }
 
 
+
 // Función que extrae el valor de cada una de las cartas elegidas,
 // Tomando los caracteres numéricos del string.
 const valorCarta = (carta) => {
@@ -66,7 +72,50 @@ const valorCarta = (carta) => {
     return (isNaN(valor)) ?
             (valor === 'A') ? 11 : 10
             : valor * 1;
+
 }
+
+
+// Turno de la computadora 
+
+const turnoComputadora = (puntosMinimos) => {
+    
+    do {
+
+        const carta = pedirCarta();
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+        marcador[1].innerHTML = puntosComputadora;
+        
+        // Crea la imagen de las cartas y las agrega al tablero
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append(imgCarta);
+
+        if (puntosMinimos > 21) {
+            break;
+        }
+
+    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos < 21));
+
+    
+    // valida los puntos y lanza el mensaje del ganador, la función setTimeout corrige
+    // el mensaje que se muestra antes que las cartas del juego.
+    setTimeout(() => {
+
+        if (puntosComputadora === puntosMinimos) {
+            alert('¡Oh, parece que nadie gana');
+        } else if (puntosMinimos > 21) {
+            alert('¡Parace que has perdido, la máquina gana!')
+        } else if (puntosComputadora > 21 || puntosJugador === 21) {
+            alert('¡Felicidades, ganaste!');
+        } else {
+            alert('¡Wow, la computadora gana!');
+        }
+
+    }, 100);
+};
+
 
 
 // Eventos
@@ -89,12 +138,49 @@ btnPedir.addEventListener('click', () => {
     if (puntosJugador > 21 ) {
         console.warn('Lo siento, perdiste');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     } else if (puntosJugador === 21) {
         console.warn('¡Genial, tienes 21 puntos!');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
+});
 
 
-    
-})
+
+// Funciones del botón detener
+btnDetener.addEventListener('click', () => {
+
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+
+});
+
+
+
+// Funciones del botón Nuevo juego que resetea los valores
+btnNuevo.addEventListener('click', () => {
+
+    console.clear();
+
+    deck = [];
+    deck = crearDeck();
+    puntosComputadora = 0;
+    puntosJugador = 0;
+    marcador[0].innerText = 0;
+    marcador[1].innerText = 0;
+
+    divCartasComputadora.innerHTML = '';
+    divCartasJugador.innerHTML = '';
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+});
+
+
+
+
 
